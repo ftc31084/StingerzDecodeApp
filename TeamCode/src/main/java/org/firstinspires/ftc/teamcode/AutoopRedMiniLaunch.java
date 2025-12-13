d43ec3dc1421806bbc1729a88c6891f23844b90e
Skeledan <121405337+TheRealSkeledan@users.noreply.github.com>
2025-12-13 13:44:41 -0500

    Create AutoopRedMiniLaunch.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoopRedMiniLaunch.java
@@ -0,0 +1,128 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class AutoopRedMiniLaunch extends LinearOpMode {
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+
+    static final double TPI = 537.7/(Math.PI * 4.094);
+    static final double TRACK_WIDTH_INCHES = 8;
+
+    @Override
+    public void runOpMode() {
+        leftFront = hardwareMap.get(DcMotor.class, "fl_drive");
+        rightFront = hardwareMap.get(DcMotor.class, "fr_drive");
+        leftBack = hardwareMap.get(DcMotor.class, "rl_drive");
+        rightBack = hardwareMap.get(DcMotor.class, "rr_drive");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            // Step 1
+            driveForwardInches(20, 0.6);
+            turnDegrees(30, 0.4);
+
+            // Step 2
+            turnDegrees(110, 0.4);
+            driveForwardInches(10, 0.5);
+
+            stopMotors();
+        }
+    }
+
+    private void driveForwardInches(double inches, double power) {
+        int ticks = (int) (inches * TPI);
+
+        leftFront.setTargetPosition(leftFront.getCurrentPosition() + ticks);
+        rightFront.setTargetPosition(rightFront.getCurrentPosition() + ticks);
+        leftBack.setTargetPosition(leftBack.getCurrentPosition() + ticks);
+        rightBack.setTargetPosition(rightBack.getCurrentPosition() + ticks);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        leftBack.setPower(power);
+        rightBack.setPower(power);
+
+        while (opModeIsActive()
+                && leftFront.isBusy()
+                && rightFront.isBusy()
+                && leftBack.isBusy()
+                && rightBack.isBusy()) {
+            idle();
+        }
+
+        stopMotors();
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private void turnDegrees(double degrees, double power) {
+        double arcLength = Math.PI * TRACK_WIDTH_INCHES * (degrees / 360.0);
+        int ticks = (int) (arcLength * TPI);
+
+        leftFront.setTargetPosition(leftFront.getCurrentPosition() - ticks);
+        leftBack.setTargetPosition(leftBack.getCurrentPosition() - ticks);
+        rightFront.setTargetPosition(rightFront.getCurrentPosition() + ticks);
+        rightBack.setTargetPosition(rightBack.getCurrentPosition() + ticks);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftFront.setPower(power);
+        leftBack.setPower(power);
+        rightFront.setPower(power);
+        rightBack.setPower(power);
+
+        while (opModeIsActive()
+                && leftFront.isBusy()
+                && leftBack.isBusy()
+                && rightFront.isBusy()
+                && rightBack.isBusy()) {
+            idle();
+        }
+
+        stopMotors();
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private void stopMotors() {
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+    }
+}
